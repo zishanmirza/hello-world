a581b239dc13281df9cd06df55eb9180d2513344
Zishan Mirza <zishanm@pm.me>
2022-03-27 12:12:50 +0000

    Added "if" statement.
    Signed-off-by: Zishan Mirza <zishanm@pm.me>

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main (int argc, char** argv)
 {
+    if (argc != 1)
+    {
+        exit (1);
+    }
+
     fprintf (stdout, "Hello, world!\n");
 
     return 0;
