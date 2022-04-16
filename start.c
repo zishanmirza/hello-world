34ad5499c56459bb8cc3a71abca77c15a04123c0
Zishan Mirza <zishanm@pm.me>
2022-04-16 21:30:48 +0000

    Updated start.c and start.h.
    Signed-off-by: Zishan Mirza <zishanm@pm.me>

--- start.c
+++ start.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "start.h"
+
 char*
 start (char* name)
 {
     char* text;
 
-    text = malloc (sizeof (char) * 1024);
-    snprintf (text, 1024, "%s: Hello, world!", name);
+    text = malloc (sizeof (char) * BUFFER);
+    snprintf (text, BUFFER, "%s: Hello, world!", name);
 
     return text;
 }
--- start.h
+++ start.h
@@ -2,6 +2,8 @@
 
 #define START_H 1
 
+#define BUFFER 1024
+
 char* start(char* name);
 
 #endif
